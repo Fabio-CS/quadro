3e40076b6091b2745618cd3433b42967f85212af
Fabio C.S. Miranda <fabio.cs3mx@gmail.com>
2015-12-14 16:50:59 -0200

    Changed database

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=global_quadro_emocional_db',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
